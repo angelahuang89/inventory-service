eb6c85909cb99cb640f089fcaa276cb0701a7623
Angela Huang <angelahuang89@gmail.com>
2017-12-12 20:42:57 -0800

    Create database connection and inventory schema

---
+++ database/postgres.js
@@ -0,0 +1,27 @@
+const Sequelize = require('sequelize');
+
+let params = {
+  host: 'localhost',
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: false,
+  dialectOptions: { ssl: true },
+};
+
+const db = new Sequelize('postgres://postgres:password@localhost:5433/bundlin');
+
+db.authenticate()
+  .then(() => console.log('Connection established'))
+  .catch(err => console.error('Connection error'));
+
+const Inventory = db.define('inventory', {
+  product_name: Sequelize.STRING,
+  product_description: Sequelize.STRING,
+  product_image: Sequelize.STRING,
+  category: Sequelize.STRING,
+  price: Sequelize.INTEGER,
+  inventory_count: Sequelize.INTEGER,
+});
+
+exports.db = db;
+exports.Inventory = Inventory;
