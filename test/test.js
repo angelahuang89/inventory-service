e063f888194dc68c32029a0894cadf2355e5de6a
Angela Huang <angelahuang89@gmail.com>
2017-12-15 17:20:45 -0800

    Write tests for server

--- test/test.js
+++ test/test.js
@@ -2,11 +2,61 @@ const should = require('chai').should();
 const expect = require('chai').expect;
 const supertest = require('supertest');
 const api = supertest('http://localhost:1337');
+const request = require('request');
+const Sequelize = require('sequelize');
+// const db = require('../database/postgres.js');
+// const dataGenerator = require('../database/dataGenerator.js');
 
-describe('Client Search', function() {
-  it('should return a 200 response', function(done) {
-    api.get('/client/search')
-    .set('Accept', 'application/json')
-    .expect(200, done);
-  })
+describe('Server', function() {
+
+  describe('Options', function() {
+    it('should return options', function() {
+      api.options('/')
+      expect('GET,POST,PUT,PATCH,GET');
+    });
+  });
+
+  describe('Client Search', function() {
+    it('should return a 200 response', function() {
+      api.get('/client/search')
+      .set('Accept', 'application/json')
+      expect(200);
+    });
+  });
+
 });
+
+// describe('Database', function() {
+//
+//   const dbConnection = '';
+//
+//   beforeEach(function(done) {
+//     dbConnection = new Sequelize('test', 'postgres', 'password', {
+//       host: 'localhost',
+//       dialect: 'postgres',
+//       protocol: 'postgres',
+//       logging: false,
+//       dialectOptions: { ssl: true },
+//   });
+//
+//   dbConnection.authenticate()
+//     .then(() => console.log('Test connection established'))
+//     .catch(err => console.error('Connection error'));
+//
+//   const table = 'testInventory';
+//
+//   afterEach(function() {
+//     dbConnection.close();
+//   });
+//
+//   it('should exist', function(done) {
+//     expect(db.inventories, done);
+//   });
+// });
+//
+// describe('Data generator',function() {
+//   it('should generate data', function() {
+//     dataGenerator.createProducts()
+//     expect('hello', done);
+//   })
+// });
