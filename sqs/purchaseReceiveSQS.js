13c5ac4452c47362a958ae07f025c8b49f0290e7
Angela Huang <angelahuang89@gmail.com>
2017-12-29 17:36:29 -0800

    Simulate Purchase Service for decrementing inventory count

---
+++ sqs/purchaseReceiveSQS.js
@@ -0,0 +1,70 @@
+const axios = require('axios');
+const Promise = require('bluebird');
+const AWS = require('aws-sdk');
+const Consumer = require('sqs-consumer');
+
+AWS.config.update({region: 'us-west-1'});
+
+// const purchaseSQS = new AWS.SQS({apiVersion: '2012-11-05'});
+
+const queueUrl = 'https://sqs.us-west-1.amazonaws.com/379538513358/purchaseQueue';
+
+// check queue on a schedule
+
+const app = Consumer.create({
+  queueUrl: queueUrl,
+  messageAttributeNames: ['ProductId, Quantity'],
+  handleMessage: (message, done) => {
+    const product = message.MessageAttributes;
+    axios.patch('http://localhost:1337/purchases', product)
+      .then(() => done())
+      .catch(error => console.log(error));
+    done();
+  }
+});
+
+app.on('error', error => {
+  console.log(error.message);
+});
+
+app.start();
+
+// const receivePurchaseInfo = () => {
+//   const params = {
+//     AttributeNames: [
+//       'SentTimestamp'
+//     ],
+//     MaxNumberOfMessages: 10,
+//     MessageAttributeNames: [
+//       'All'
+//     ],
+//     QueueUrl: queueUrl,
+//     VisibilityTimeout: 0,
+//     WaitTimeSeconds: 0,
+//   };
+//
+//   purchaseSQS.receiveMessage(params, (error, data) => {
+//     if (error) {
+//       console.log('Client queue receive error', error);
+//     } else if (data.Messages) {
+//       data.Messages.forEach(message => {
+//         product = message.MessageAttributes;
+//         axios.patch('http://localhost:1337/purchases', product)
+//           .then(response => {
+//             const deleteParams = {
+//               QueueUrl: queueUrl,
+//               ReceiptHandle: message.ReceiptHandle,
+//             };
+//             purchaseSQS.deleteMessage(deleteParams, (error, data) => {
+//               if (error) {
+//                 console.log('Client queue delete error', error);
+//               } else {
+//                 console.log('Client queue message deleted', data);
+//               }
+//             });
+//       });
+//     }
+//   });
+// };
+//
+// exports.receivePurchaseInfo = receivePurchaseInfo;
---
+++ sqs/purchaseSendSQS.js
@@ -0,0 +1,41 @@
+const AWS = require('aws-sdk');
+AWS.config.update({region: 'us-west-1'});
+
+const purchaseSQS = new AWS.SQS({apiVersion: '2012-11-05'});
+
+const queueUrl = 'https://sqs.us-west-1.amazonaws.com/379538513358/purchaseQueue';
+
+const sendPurchase = () => {
+  const product = {};
+  product.id = Math.floor(Math.random() * 2000000);
+  product.quantity = Math.floor(Math.random() * 10000);
+
+  const { id, quantity } = product;
+
+  const params = {
+    MessageAttributes: {
+      'ProductId': {
+        DataType: 'Number',
+        StringValue: `${id}`,
+      },
+      'Quantity': {
+        DataType: 'Number',
+        StringValue: `${quantity}`,
+      },
+    },
+    MessageBody: 'Information for decreasing product count',
+    QueueUrl: queueUrl,
+  };
+
+  purchaseSQS.sendMessage(params, (error, data) => {
+    if (error) {
+      console.log('Purchase queue send error', error);
+    } else {
+      console.log('Purchase queue send success', data.MessageId);
+      return;
+    }
+  });
+}
+
+exports.purchaseSQS = purchaseSQS;
+exports.sendPurchase = sendPurchase;
